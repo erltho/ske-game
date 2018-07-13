8c56daf54036314018dbcc3bec3167e1c2df60f9
Kim Lindtner <kim.lindtner@skatteetaten.no>
2018-07-13 15:41:43 +0200

    merging started

--- src/app.js
+++ src/app.js
@@ -4,24 +4,74 @@ import {startGame, restart} from './game'
 import SkeBasis from 'aurora-frontend-react-komponenter/SkeBasis';
 import Button from 'aurora-frontend-react-komponenter/Button';
 import GameLayout from './layout'
+import loadMainMenu from './mainMenu'
+import Underscore from 'underscore';
 
 import './index.css'
 
 
 class Game extends React.Component {
 
-
+/*
   componentDidMount() {
     startGame()
   }
-
+*/
   render() {
     return (
       <GameLayout>
         <div id="game"/>
-        <Button buttonType="primary" onClick={restart}>Start Game</Button>
+        <Button buttonType="primary" onClick={startGame}>Start Game</Button>
       </GameLayout>
     )
+  }
+
+}
+
+class MainMenu extends React.Component {
+
+
+    componentDidMount() {
+      loadMainMenu()
+    }
+
+  render() {
+    return <GameLayout>
+      <div className="ske-layout__body">
+        <main className="ske-layout__kjerne">
+          <div className="contentWrapper">
+            <div className="content colorHoved">
+              <img src={require("../src/assets/img/Skattepumpa.png")} height="" className="imgCenter"/>
+              <div className="highScore scoreList"></div>
+              <form id='nameForm'>
+                <div className="inputList">
+                  <ul className="input-list style-2 clearfix">
+                    <li>
+                      <input id='nameField' type="text" placeholder="Ditt navn"/>
+                    </li>
+                    <li>
+                      <input id='numberField' type="text" placeholder="Ditt telefonnummer"/>
+                    </li>
+                  </ul>
+                </div>
+                <div style={{position: 'relative'}} className="errorMsg">
+                  <div id="strError" className="errorMsgBlock" style={{color: "red"}}></div>
+                  <div style={{bottom: "0", position: "absolute", color: "red"}} id="nrError"
+                       className="errorMsgBlock"></div>
+                </div>
+              </form>
+              <div
+                style={{marginBottom: "2%", marginLeft: "2%", marginTop: "8.8%", zIndex: "10", position: "absolute"}}>
+                <img src={require("../src/assets/img/2017/ForsideGubbe.png")} height="200px"/>
+              </div>
+            </div>
+            {/* TODO: Knapp som sjekker input*/}
+          </div>
+        </main>
+      </div>
+      <div id="game"/>
+      <Button buttonType="primary" onClick={startGame}>Start Game</Button>
+    </GameLayout>
   }
 
 }
@@ -29,6 +79,6 @@ class Game extends React.Component {
 
 ReactDOM.render(
   <SkeBasis>
-  <Game />
+    <MainMenu/>
   </SkeBasis>
   , document.getElementById('root'));
---
+++ src/mainMenu.js
@@ -0,0 +1,133 @@
+import Underscore from 'underscore';
+
+
+
+function loadMainMenu() {
+  localStorage.setItem("BrukersNavn", "");
+  localStorage.setItem("BrukersNummer", "");
+  localStorage.setItem("buttonMashScore", 0);
+  localStorage.setItem("reactionGameScore", 0);
+  localStorage.setItem("totalScore", 0);
+
+  if (localStorage.getItem("scoreBoard") === null) {
+    localStorage.setItem("scoreBoard", "[]");
+    console.log(localStorage.scoreBoard);
+  }
+
+  plotHigh();
+}
+
+function inputValidator() {
+  var nameField = document.getElementById('nameField').value;
+  var numberField = document.getElementById('numberField').value;
+
+  var nameInput = document.getElementById('strError');
+  var numberInput = document.getElementById('nrError');
+
+
+  var scores1 = JSON.parse(localStorage.scoreBoard);
+  var validerNummer = _.where(scores1, {nummer: numberField});
+
+
+  var strRegex = /^[\w._øæå ]+$/;
+  var numRegex = /^\d{8}$/;
+
+  var navntxt = "Dette er ikke et gyldig navn";
+  var nummertxt = "Dette er ikke et gyldig telefonnummer";
+  var nummerNotValid = "Dette nummeret er allerede registrert";
+
+
+  if (nameField.match(strRegex) && numberField.match(numRegex) && validerNummer.length === 0) {
+    localStorage.setItem("BrukersNavn", nameField);
+    localStorage.setItem("BrukersNummer", numberField);
+    window.location.replace('buttonMash.html');
+  }
+
+  if (!nameField.match(strRegex)) {
+    nameInput.textContent = navntxt;
+    document.getElementById("nameField").style.borderColor = "red";
+  } else {
+    nameInput.textContent = "";
+    document.getElementById("nameField").style.borderColor = "grey";
+  }
+
+  if (!numberField.match(numRegex)) {
+    numberInput.textContent = nummertxt;
+    document.getElementById("numberField").style.borderColor = "red";
+  } else if (validerNummer.length > 0) {
+    numberInput.textContent = nummerNotValid;
+    document.getElementById("numberField").style.borderColor = "red";
+  } else {
+    numberInput.textContent = "";
+    document.getElementById("numberField").style.borderColor = "grey";
+  }
+}
+
+function getUserName() {
+  var nameField = document.getElementById('nameField').value;
+  var navnResultat = document.getElementById('navnResultat');
+  console.log(nameField);
+
+  if (nameField.length < 0) {
+    navnResultat.textContent = 'Navnet ditt må bestå av minst 3 bokstaver';
+
+  } else {
+    localStorage.setItem("BrukersNavn", nameField);
+
+  }
+}
+
+function getNumber() {
+  var numberField = document.getElementById('numberField').value;
+  var nummerResultat = document.getElementById('nummerResultat');
+  console.log(numberField);
+
+  localStorage.setItem("BrukersNummer", numberField);
+
+}
+
+var subButton = document.getElementById('subButton');
+
+function plotHigh() {
+  var scores = JSON.parse(localStorage.scoreBoard);
+
+  var scores =  _.sortBy(scores, function (o) {
+    return parseInt(o.totalScore);
+  }).reverse();
+
+  for (var i = 0; i < 5; i++) {
+
+    $('.highScore').append($('<div />', {
+      class: 'myRow lineHS' + i
+    }));
+
+    if (i % 2 == 0) {
+      $('.lineHS' + i).css('background-color', '#e8d6dc');
+    }
+
+    for (var j = 0; j < 3; j++) {
+
+      var str;
+
+      if (scores[i] == null) {
+        str = "--"
+      } else {
+        if (j == 0) {
+          str = i + 1;
+        } else if (j == 1) {
+          str = scores[i].navn;
+        } else {
+          str = scores[i].totalScore;
+        }
+      }
+
+      $('.lineHS' + i).append($('<div />', {
+        class: 'col colHS' + j,
+        text: str
+      }));
+    }
+  }
+}
+
+
+export default loadMainMenu;
